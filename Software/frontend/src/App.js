20b35952f46aa55854e209c67b20f9c6c1417839
Thomas Hall <tjh2822@g.rit.edu>
2020-01-03 16:42:21 -0500

    added models to backend, front end for gps, accel gyro, battery

--- Software/frontend/src/App.js
+++ Software/frontend/src/App.js
@@ -5,49 +5,42 @@ import React, { Component } from "react";
 import GpsView from './components/gps/gps_view.js';
 import AccGyroView from './components/accgyro/accgyro_view.js';
 import BatteryView from './components/battery/battery_view.js';
+import VideoPictureView from './components/video/video_picture_view.js';
+import VideoDataView from './components/video/video_data_view.js';
 
 class App extends Component {
 
   render() {
     return (
       <div className="root">
+        
         <div className="row">
-          <div className="fifth container_item container_top">
-            <GpsView />
-          </div>
-          <div className="fifth container_item container_top">
-            <GpsView />
-          </div>
-          <div className="fifth container_item container_top">
-            <GpsView />
-          </div>
-          <div className="fifth container_item container_top">
-            <GpsView />
+          <div className="navbar">
+            <p>navbar</p>
           </div>
         </div>
+
         <div className="row">
-          <div className="quarter container_item">
-            <AccGyroView />
+          <div className="fifth">
+            <div className="container_item container_exclusive">
+              <GpsView />
+            </div>
           </div>
-          <div className="quarter container_item">
-            <AccGyroView />
+          <div className="videocontainter">
+            <div className="container_item container_exclusive">
+              <VideoPictureView />
+            </div>
+            <div className="container_item container_exclusive">
+              <VideoDataView />
+            </div>    
           </div>
-          <div className="quarter container_item">
-            <AccGyroView />
-          </div>
-          <div className="quarter container_item">
-            <AccGyroView />
-          </div>
-        </div>
-        <div className="row">
-          <div className="container_item container_bottom">
-            <BatteryView />
-          </div>
-          <div className="container_item container_top">
-            <GpsView />
-          </div>
-          <div className="container_item container_top">
-            <GpsView />
+          <div className="fifth">
+            <div className="container_item container_exclusive">
+              <AccGyroView />
+            </div>
+            <div className="container_item container_exclusive">
+              <BatteryView />
+            </div>
           </div>
         </div>
       </div>
---
+++ Software/frontend/src/components/video/video_data_view.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import axios from "axios";
+
+class VideoDataView extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          videoList: [],
+        };
+      }
+      
+    // The componentDidMount() method runs after the component output has been rendered to the DOM.
+    componentDidMount() {
+    this.refreshList();
+
+    // set auto refresh to 2000 ms
+    setInterval(this.refreshList, 2000);
+    }
+
+    // This function refreshes the list of video items
+    refreshList = () => {
+    axios
+        .get("http://localhost:8000/api/video/")
+        .then(res => this.setState({ videoList: res.data }))
+        .catch(err => console.log(err));
+    };
+    
+    // Gets the latest
+    getLatest(list){
+
+        if(list.length === 0){
+            return -1;
+        }
+    
+        var earliest = list[0];
+    
+        for (const item of list.entries()){
+            if(item.time < earliest.time){
+                earliest = item
+            }
+        }
+        return earliest;
+    };
+
+    // This function returns the div that we want to render
+    renderItems = () => {
+
+        const latestVideo = this.getLatest(this.state.videoList);
+        
+        if(latestVideo === -1){
+            return <span>no video data</span>;
+        }
+        else{           
+            return <div>
+                <table>
+                    <tr>
+                        <td>Voltage: {latestVideo.voltage}</td>
+                    </tr>
+                    <tr>
+                        <td>Amperage: {latestVideo.amperage}</td>
+                    </tr>
+                </table>
+                <span>Last Recieved: {latestVideo.date} {latestVideo.time}</span>
+                </div>
+        }
+    };
+    
+    handleSubmit = item => {
+        this.toggle();
+        if (item.id) {
+            axios
+            .put(`http://localhost:8000/api/video/${item.id}/`, item)
+            .then(res => this.refreshList());
+            return;
+        }
+        axios
+            .post("http://localhost:8000/api/video/", item)
+            .then(res => this.refreshList());
+    };
+
+    // Returns jsx to render the item in react
+    render() {
+        return (
+            <div>
+                {this.renderItems()}
+            </div>
+        );
+    }
+}
+
+export default VideoDataView
